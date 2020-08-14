060c5fdd5875967b5e7706d543c96713286962ab
Dalton Okechukwu <okechukwu@whitewhale.ai>
2020-08-14 16:43:48 -0600

    interceptors and axios header configurations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json"; // already default
+
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    // console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (error) => {
+    // console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+// ejecting interceptors
+// var myInterceptor = axios.interceptors.request.use(request => {/*...*/});
+// axios.interceptors.request.eject(myInterceptor);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
